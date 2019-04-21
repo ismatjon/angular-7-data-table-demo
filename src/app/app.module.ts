73ee401036cfaf5adede90bec5c99ee589f6e67e
Ismatjon Qalandarov <kalandarov.ismat@gmail.com>
2019-04-21 16:32:48 +0500

    refactored a bit, formatted files, and replaced glyphicons with font-awesome icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { DataTableModule } from './data-table';
+
+import { DataTableDemo1 } from './demo1/data-table-demo1';
+import { DataTableDemo2 } from './demo2/data-table-demo2';
+import { DataTableDemo3 } from './demo3/data-table-demo3';
+// import { DataTableDemo1Remote } from './demo1/data-table-demo1-remote';
+import { RemoteService } from './demo1/data-table-demo1-remote-service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent, DataTableDemo1, DataTableDemo2, DataTableDemo3],
+  imports: [BrowserModule, FormsModule, DataTableModule],
+  providers: [RemoteService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/demo2/data-table-demo2.ts
@@ -0,0 +1,37 @@
+import { Component, ViewChild } from '@angular/core';
+import { DataTable, DataTableResource } from '../data-table';
+import { cars } from './data-table-demo2-data';
+
+@Component({
+  selector: 'data-table-demo-2',
+  templateUrl: './data-table-demo2.html'
+})
+export class DataTableDemo2 {
+  constructor() {
+    this.rowColors = this.rowColors.bind(this);
+
+    this.carResource.count().then(count => (this.carCount = count));
+  }
+
+  carResource = new DataTableResource(cars);
+  cars = [];
+  carCount = 0;
+
+  @ViewChild(DataTable) carsTable: DataTable;
+
+  yearLimit = 1999;
+
+  reloadCars(params) {
+    this.carResource.query(params).then(cars => (this.cars = cars));
+  }
+
+  // custom features:
+
+  carClicked(car) {
+    alert(car.model);
+  }
+
+  rowColors(car) {
+    if (car.year >= this.yearLimit) return 'rgb(255, 255, 197)';
+  }
+}
