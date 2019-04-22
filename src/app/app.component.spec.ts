bfb9e9c428a327ad679f7344ef72ed8716503091
Ismatjon Qalandarov <kalandarov.ismat@gmail.com>
2019-04-22 08:34:05 +0500

    renamed package name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-data-table'`, () => {
+  it(`should have as title 'angular-data-tables'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-data-table');
+    expect(app.title).toEqual('angular-data-tables');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-data-table!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-data-tables!');
   });
 });
